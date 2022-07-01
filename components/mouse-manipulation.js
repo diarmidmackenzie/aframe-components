92c802ce00bee5fa7733de318a8e800021112c4d
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-07-01 21:36:13 +0100

    Distinct functions for left click and right/middle click

--- components/mouse-manipulation.js
+++ components/mouse-manipulation.js
@@ -74,6 +74,7 @@ AFRAME.registerComponent('mouse-manipulation', {
     
         // variable to track any grabbed element
         this.grabbedEl = null;
+        this.grabbedElWorldPosition = new THREE.Vector3();
 
         // We create 2 children beneath the camera
         // - cursorTracker.  This is set up to match the orientation of the cursor
@@ -84,49 +85,121 @@ AFRAME.registerComponent('mouse-manipulation', {
         this.camera.appendChild(this.cursorTracker)
 
         // A container for any entity that can be grabbed.
-        // For mouse controls, this is a child of the camera.
+        // For mouse controls, this is a child of the cursor tracker.
         // (this helps with move in/out, rotation etc. of grabbed entity)
-        this.contactPoint = document.createElement('a-entity')
-        this.contactPoint.setAttribute('id', `${this.el.id}-contact-point`)
-        this.cursorTracker.appendChild(this.contactPoint)
+        this.cursorContactPoint = document.createElement('a-entity')
+        this.cursorContactPoint.setAttribute('id', `${this.el.id}-cursor-contact-point`)
+        this.cursorTracker.appendChild(this.cursorContactPoint)
+
+        // A container for any entity that can be grabbed.
+        // This is a child of the camera, for controls where the object
+        // shouldn't follow the mouse pointer, e.g. rotation.
+        // (this helps with move in/out, rotation etc. of grabbed entity)
+        this.cameraContactPoint = document.createElement('a-entity')
+        this.cameraContactPoint.setAttribute('id', `${this.el.id}-camera-contact-point`)
+        this.camera.appendChild(this.cameraContactPoint)
 
         // for working
         this.vector1 = new THREE.Vector3()
         this.vector2 = new THREE.Vector3()
 
         this.mouseWheel = this.mouseWheel.bind(this)
+        this.windowMouseUp = this.windowMouseUp.bind(this)
+        this.windowMouseDown = this.windowMouseDown.bind(this)
         window.addEventListener('wheel', this.mouseWheel);
+        window.addEventListener('mouseup', this.windowMouseUp);
+        window.addEventListener('mousedown', this.windowMouseDown);
+        window.addEventListener('contextmenu', event => event.preventDefault());
+    },
+
+    windowMouseDown(evt) {
+
+        // we are looking for the original mouseEvent, which has details of buttons pressed
+        // And we need to have registered an element to be grabbed.
+        if (evt.buttons === undefined) return;
+        if (!this.grabbedEl) return;
+
+        if (evt.buttons % 2) {
+            // left button is pressed (either just pressed or already down) 
+            // - grab to cursor contact point
+            this.grabElToContactPoint(this.cursorContactPoint,
+                                          `#${this.el.id}-cursor-contact-point`)
+
+        }
+        else {
+            // right or middle button - grab to camera contact point
+            this.grabElToContactPoint(this.cameraContactPoint,
+                                      `#${this.el.id}-camera-contact-point`)
+        }
+
+        evt.preventDefault()
+    },
+
+    windowMouseUp(evt) {
+        // we are looking for the original mouseEvent, which has details of buttons pressed
+        // And we need to have a grabbed element.
+        if (evt.buttons === undefined) return;
+        if (!this.grabbedEl) return;
+
+        if (evt.buttons % 2) {
+            // left button is still down
+            // leave attached to cursor contact point.
+        }
+        else if (evt.buttons === 0){
+            // no button now pressed.
+            this.releaseEl()
+
+        }
+        else if (evt.button === 1) {
+            // left button released, but right or middle button still down 
+            // - grab to camera contact point
+            grabEntityToContactPoint(this.cameraContactPoint,
+                `#${this.el.id}-camera-contact-point`)
+        }
+
+        evt.preventDefault()
     },
 
     remove() {
         window.removeEventListener('wheel', this.mouseWheel);
     },
 
+    // records details of grabbed ovject, but actual grabbing is deferred to be handled on MouseEvent
+    // based on detail about which button is pressed (not avalable on this event)
     mouseDown(evt) {
   
-        console.assert(!this.grabbedEl)
-    
         const intersections = this.getIntersections(evt.target);
     
         if (intersections.length === 0)  return;
     
         const element = intersections[0]
-    
-        const intersectionData = this.el.components.raycaster.getIntersection(element)
-    
-        // reparent element to the camera.
-        this.grabbedEl = element.components['clickable'].target
-        const grabbedPoint = this.contactPoint.object3D.parent.worldToLocal(intersectionData.point)
-        this.contactPoint.object3D.position.copy(grabbedPoint)
-        this.grabbedEl.setAttribute('object-parent', 'parent', `#${this.el.id}-contact-point`)
+        const newGrabbedEl = element.components['clickable'].target
+
+        if (this.grabbedEl && 
+            this.grabbedEl !== newGrabbedEl) {
+            console.warn("Grabbed 2nd element without releasing the first:", newGrabbedEl.id, this.grabbedEl.id)
+        }
+
+        this.grabbedEl = newGrabbedEl
+        this.grabbedEl.object3D.getWorldPosition(this.grabbedElWorldPosition)
+    },
+
+    grabElToContactPoint(contactPoint, contactPointSelector) {
+
+        // set up a contact point at the position of the grabbed entity
+        const pos = contactPoint.object3D.position
+        pos.copy(this.grabbedElWorldPosition)
+        contactPoint.object3D.parent.worldToLocal(pos)
+        this.grabbedEl.setAttribute('object-parent', 'parent', contactPointSelector)
+    },
+
+    releaseEl() {
+        this.grabbedEl.setAttribute('object-parent', 'parent', `#${this.data.defaultParent.id}`)
+        this.grabbedEl = null
     },
 
     mouseUp() {
-  
-        if (!this.grabbedEl) return
-    
-        this.grabbedEl.setAttribute('object-parent', 'parent', `#${this.data.defaultParent.id}`)
-        this.grabbedEl = null
+        // all work done on MouseEvent, where we have detail as to *which* button is pressed.
     },
 
     getIntersections(cursorEl) {
@@ -144,7 +217,7 @@ AFRAME.registerComponent('mouse-manipulation', {
     // Implements moving out or in (in = -ve)
     moveOut(timeDelta) {
       const scalar = Math.pow(this.moveSpeed, timeDelta/1000);
-      this.contactPoint.object3D.position.multiplyScalar(scalar)
+      this.cursorContactPoint.object3D.position.multiplyScalar(scalar)
     },
   
       /*
@@ -159,10 +232,10 @@ AFRAME.registerComponent('mouse-manipulation', {
       }
   
       if (this.el.is("rotating-y-plus")) {
-        this.contactPoint.object3D.rotation.y += timeDelta * this.rotateRate / 1000;
+        this.cursorContactPoint.object3D.rotation.y += timeDelta * this.rotateRate / 1000;
       }
       else if (this.el.is("rotating-y-minus")) {
-        this.contactPoint.object3D.rotation.y -= timeDelta * this.rotateRate / 1000;
+        this.cursorContactPoint.object3D.rotation.y -= timeDelta * this.rotateRate / 1000;
       }
     }*/
 });
