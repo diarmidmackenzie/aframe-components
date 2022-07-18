f852b00bba6314d9ac45ed2b02c23d057784075f
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-07-18 14:09:36 +0100

    Show controls for controller buttons.

--- components/desktop-vr-controller.js
+++ components/desktop-vr-controller.js
@@ -11,6 +11,8 @@ AFRAME.registerComponent('desktop-vr-controller', {
 
         this.el.sceneEl.addEventListener('enter-vr', this.simulateController)
         this.el.sceneEl.addEventListener('exit-vr', this.removeController)
+
+        this.labels = []
     },
 
     simulateController() {
@@ -43,16 +45,88 @@ AFRAME.registerComponent('desktop-vr-controller', {
                                  'ControlLeft': 'grip',
                                  'KeyA': 'abutton',
                                  'KeyB': 'bbutton'}
+
         }
         else {
             this.keyBindings = {'ShiftRight' : 'trigger',
                                 'ControlRight': 'grip',
                                 'KeyX': 'xbutton',
                                 'KeyY': 'ybutton'}
+
+            this.createLabel("R-Shift", "trigger")
+            this.createLabel("R-Ctrl", "grip")
+            this.createLabel("A", "abutton")
+            this.createLabel("B", "bbutton")
         }
 
         window.addEventListener('keyup', this.keyUp)
         window.addEventListener('keydown', this.keyDown)
+    },
+
+    createLabel(text, positionIdentifier) {
+
+        var pos;
+        var offset;
+    
+        switch (positionIdentifier) {
+            case "trigger":
+                pos = "-0.015 -0.02 0.01"
+                offset = "-0.1 0 -0.2"
+                break;
+
+            case "grip":
+                pos = "-0.015 -0.02 0.06"
+                offset = "-0.25 0 0"
+
+                break;
+
+            case "abutton":
+                pos = "-0.017 0.005 0.045"
+                offset = "0.2 0.2 0"
+                break;
+
+            case "bbutton":
+                pos = "-0.022 0.005 0.03"
+                offset = "0.15 0.2 -0.1"
+                break;
+
+            default:
+                console.error("unexpected position identifier")
+                break;
+        }
+
+        const anchor = document.createElement("a-entity")
+        anchor.setAttribute("position", pos)
+        anchor.setAttribute("label-anchor", `offsetVector: ${offset}; lineColor: yellow`)
+        
+        const label = document.createElement("a-entity")
+        label.setAttribute("label", {overwrite: true, forceDesktopMode: true});
+        anchor.appendChild(label)
+
+        // add all in one go so that label reference can be resolved by anchor
+        this.el.appendChild(anchor)
+
+        const button = document.createElement('a-plane')
+        button.setAttribute("width", 0.1)
+        button.setAttribute("height", 0.1)
+        button.setAttribute("color", "black")
+        button.setAttribute("text", `value:${text};
+                                     color: white;
+                                     wrapCount: ${text.length + 2};
+                                     align: center;
+                                     anchor: center`)
+        label.appendChild(button)
+
+        this.labels.push(anchor)
+    },
+
+    removeLabels() {
+
+        this.labels.forEach((label) => {
+            label.parentNode.removeChild(label);
+        })
+
+        this.labels = []
     },
 
     removeController() {
@@ -71,10 +145,12 @@ AFRAME.registerComponent('desktop-vr-controller', {
 
         window.removeEventListener('keyup', this.keyUp)
         window.removeEventListener('keydown', this.keyDown)
+
+        this.removeLabels()
     },
 
     keyUp(evt) {
-        
+
         const binding = this.keyBindings[evt.code]
 
         if (binding) {
@@ -93,4 +169,5 @@ AFRAME.registerComponent('desktop-vr-controller', {
         }
     }
 })
+
 
