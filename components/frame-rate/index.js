e21e7fca096b9f74b985e8735b57acc121e9f92e
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-01-12 16:51:23 +0000

    Documentation, bug fixes, usability improvements

--- components/frame-rate/index.js
+++ components/frame-rate/index.js
@@ -1,16 +1,27 @@
+/* Component to update the target frame rate for an XR device, as per the WebXr Space.
+   API Reference: https://www.w3.org/TR/webxr/#dom-xrsession-updatetargetframerate
+*/
 
 AFRAME.registerComponent('frame-rate', {
   schema: {
-    type: 'number', default: '72'
+    type: 'number', default: 72
   },
 
   init() {
-    this.el.sceneEl.addEventListener('enterVR', this.update)
+    this.update = this.update.bind(this)
+    this.el.sceneEl.addEventListener('enter-vr', this.update)
   },
 
   update() {
 
-    const renderer = this.el.sceneEl.renderer;
+    const scene = this.el.sceneEl
+    if (!scene.is('vr-mode')) {
+      // Don't try to set frame rate when not in VR mode.
+      // The frame rate will be set on entry to VR.
+      return;
+    }
+
+    const renderer = scene.renderer;
     if (!renderer.xr || !renderer.xr.getSession) {
       console.warn(`Unabled to request unsupported frame rate of ${this.data}.  No XR Session found.`)
       return;
@@ -32,6 +43,23 @@ AFRAME.registerComponent('frame-rate', {
     else {
       console.warn(`Unabled to request unsupported frame rate of ${this.data}.  Supported frame rates: ${rates}`)
     }
+  },
+
+  getSupportedFrameRates() {
+
+    const renderer = this.el.sceneEl.renderer;
+    if (!renderer.xr || !renderer.xr.getSession) {
+      console.warn(`Unabled to get list of supported Frame Rates.  No XR Session found.`)
+      return;
+    };
+
+    const session = renderer.xr.getSession()
+    if (!session) {
+      console.warn(`Unable to get list of supported Frame Rates.  No XR Session found.`)
+      return;
+    };
+
+    return session.supportedFrameRates;
   },
 
   getFrameRate() {
