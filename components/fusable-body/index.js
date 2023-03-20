f3e60574f883c97f422d82a9c947efacf1e7ddf9
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-03-20 17:11:27 +0000

    Use bounding box to compute center for container

--- components/fusable-body/index.js
+++ components/fusable-body/index.js
@@ -2,6 +2,9 @@ if (!AFRAME.components['object-parent']) require('aframe-object-parent')
 if (!AFRAME.components['raycast-target']) require('aframe-raycast-target')
 if (!AFRAME.components['graph-node']) require('aframe-graph')
 
+const _box = new THREE.Box3()
+const _tempParent = new THREE.Object3D()
+
 AFRAME.registerSystem('fusable-body', {
 
   init() {
@@ -85,11 +88,11 @@ AFRAME.registerSystem('fusable-body', {
 
     // source & target containers can be determined from 1st nodes of component'
     const sourceContainer = this.nodeMap[fromComponent[0]].object3D.parent.el
-    const targetContainerId = this.nodeMap[toComponent[0]].object3D.parent.el.id
+    const targetContainer = this.nodeMap[toComponent[0]].object3D.parent.el
 
     fromComponent.forEach((uuid) => {
       const node = this.nodeMap[uuid]
-      const targetContainerSelector = `#${targetContainerId}`
+      const targetContainerSelector = `#${targetContainer.id}`
       node.setAttribute('object-parent', {parent: targetContainerSelector})
       
       // !! raycast-target doesn't support updates - TO FIX !!
@@ -99,6 +102,9 @@ AFRAME.registerSystem('fusable-body', {
 
     // sourceContainer is empty & no longer needed.
     this.destroyContainer(sourceContainer)
+
+    // recenter target container to reflect new content
+    this.recenterContainer(targetContainer)
   },
 
   componentsSplit(evt) {
@@ -106,18 +112,20 @@ AFRAME.registerSystem('fusable-body', {
     component1 = evt.detail.thisComponent
     component2 = evt.detail.otherComponent
 
-    let componentToMove
+    let componentToMove, componentToLeave
 
     if (component1.length >= component2.length) {
       componentToMove = component2
+      componentToLeave = component1
     }
     else {
       componentToMove = component1
+      componentToLeave = component2
     }
 
     // where to position containers?
-    const container = this.createContainer(evt.target, "kinematic") 
-    const targetContainerSelector = `#${container.id}`
+    const newContainer = this.createContainer(evt.target, "kinematic") 
+    const targetContainerSelector = `#${newContainer.id}`
     
     componentToMove.forEach((uuid) => {
       const node = this.nodeMap[uuid]
@@ -127,6 +135,36 @@ AFRAME.registerSystem('fusable-body', {
       node.removeAttribute('raycast-target')
       node.setAttribute('raycast-target', targetContainerSelector)
     })
+
+    const unmovedContainer = this.nodeMap[componentToLeave[0]].object3D.parent.el
+    this.recenterContainer(newContainer)
+    this.recenterContainer(unmovedContainer)
+  },
+
+  // recenters the container based on the bounding box of its current contents.
+  // should be called whenever component conta
+  recenterContainer(containerEl) {
+
+    const container = containerEl.object3D
+    _box.setFromObject(containerEl.object3D)
+    const scene = this.el.sceneEl.object3D
+    scene.add(_tempParent)
+
+    // store off all children.  Work backwards as children are removed as we go.
+    const children = container.children
+    for (let ii = children.length - 1; ii >= 0; ii--) {
+      _tempParent.attach(children[ii])
+    }
+
+    // reposition container
+    _box.getCenter(container.position)
+    container.parent.worldToLocal(container.position)
+
+    // restore children.  Work backwards as children are removed as we go.
+    const tempChildren = _tempParent.children
+    for (let ii = tempChildren.length - 1; ii >= 0; ii--) {
+      container.attach(tempChildren[ii])
+    }
   }
 })
 
