dc450ad291592b2e40f61cecc9566334a0b0877b
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-03-30 19:02:17 +0100

    Support updates to polygon-wireframe properties

--- components/polygon-wireframe/index.js
+++ components/polygon-wireframe/index.js
@@ -10,36 +10,50 @@ AFRAME.registerComponent("polygon-wireframe", {
     },
 
     init() {
+      const baseGeometry = this.el.getObject3D('mesh').geometry
+      if (!baseGeometry) {
+          console.warn("polygon-wireframe: no base geometry found")
+      };
 
-        const baseGeometry = this.el.getObject3D('mesh').geometry
-        if (!baseGeometry) {
-            console.warn("polygon-wireframe: no base geometry found")
-        };
+      this.edges = new THREE.EdgesGeometry( baseGeometry );
+    },
 
-        const edges = new THREE.EdgesGeometry( baseGeometry );
-        var material;
+    update() {
+
+        const oldMaterial = this.material
+        const oldLine = this.line
+
         if (!this.data.dashed) {
-            material = new THREE.LineBasicMaterial( { color: this.data.color } )
+            this.material = new THREE.LineBasicMaterial( { color: this.data.color } )
         }
         else {
-            material = new THREE.LineDashedMaterial( { color: this.data.color,
-                                                       dashSize: this.data.dashSize,
-                                                       gapSize: this.data.gapSize,
-                                                       scale: this.data.dashScale } )
+            this.material = new THREE.LineDashedMaterial( { color: this.data.color,
+                                                            dashSize: this.data.dashSize,
+                                                            gapSize: this.data.gapSize,
+                                                            scale: this.data.dashScale } )
         }
 
         if (this.data.onTop) {
+          const material = this.material
           material.depthWrite = false
           material.depthTest = false
           material.toneMapped = false
           material.transparent = true
         }
         
-        const line = new THREE.LineSegments( edges, material );        
-        line.computeLineDistances();
+        this.line = new THREE.LineSegments( this.edges, this.material );
+        this.line.computeLineDistances();
 
-        this.el.object3D.add( line );
+        this.el.object3D.add( this.line );
 
         this.el.getObject3D('mesh').visible = false;
+
+        // dispose of old material & line
+        if (oldMaterial) {
+          oldMaterial.dispose()
+        }
+        if (oldLine) {
+          oldLine.removeFromParent()
+        }
     }
 })
