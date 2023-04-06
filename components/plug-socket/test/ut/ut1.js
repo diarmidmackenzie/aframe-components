8677ee30e6c55a66b5592de3fd5c66f746857168
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-04-06 16:37:13 +0100

    Multiple basic a-frame tests

--- components/plug-socket/test/ut/ut1.js
+++ components/plug-socket/test/ut/ut1.js
@@ -1,5 +1,18 @@
 QUnit.module('add', function() {
-  QUnit.test('two numbers', function(assert) {
-    assert.equal(add(1, 2), 3);
+  QUnit.test('create box', function(assert) {
+    const scene = document.querySelector('a-scene')
+    const box = document.createElement('a-box')
+    scene.appendChild(box)
+    
+    assert.equal(box.object3D.position.x, 0);
   });
+
+  QUnit.test('create box 2', function(assert) {
+    const scene = document.querySelector('a-scene')
+    const box = document.createElement('a-box')
+    scene.appendChild(box)
+    
+    assert.equal(box.object3D.position.x, 0);
+  });
+
 });
