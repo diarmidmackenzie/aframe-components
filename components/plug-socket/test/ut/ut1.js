648069be78c9f6760b93f455c3857305a50f6261
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-04-07 14:25:25 +0100

    Additional UT

--- components/plug-socket/test/ut/ut1.js
+++ components/plug-socket/test/ut/ut1.js
@@ -297,5 +297,20 @@ QUnit.module('add', function() {
     simplePlugSocketTest(assert, options)
   });
 
+  QUnit.test('plug rotated 10 degrees X axis connects & aligns', function(assert) {
+
+    const options = {
+      snapDistance: 0.2,
+      pos1: '0 1.1 0',
+      pos2: '0 0 0',
+      rot2: '10 0 0',
+      sockPos: '0 -0.5 0',
+      plugPos: '0 0.5 0',
+      finalPos1: '0 1.1 0',
+      finalPos2: '0 0.1 0',
+     }
+    simplePlugSocketTest(assert, options)
+  });
+
 
 });
