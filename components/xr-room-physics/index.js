d4b2879796db28b188459c315bb332b838faa0d0
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-05-04 08:48:10 +0100

    Correctly position planes for both Ammo and Cannon physics

--- components/xr-room-physics/index.js
+++ components/xr-room-physics/index.js
@@ -3,7 +3,23 @@ import { RealityAccelerator } from 'ratk';
 
 AFRAME.registerSystem('xr-room-physics', {
 
+  schema: {
+    // when enabled, planes are rendered with random colors to aid in debugging.
+    debug: {default: false},
+
+    // depth (i.e. thickness) to use for walls and surfaces
+    depth: {default: 0.1},
+  },
+
   init() {
+
+    if (this.el.sceneEl.getAttribute('physics').driver === "ammo") {
+      this.driver = "ammo"
+    }
+    else {
+      this.driver = "cannon"
+    }
+
     const scene = this.el.sceneEl.object3D
     const renderer = this.el.sceneEl.renderer
     const ratk = new RealityAccelerator(renderer.xr);
@@ -14,8 +30,8 @@ AFRAME.registerSystem('xr-room-physics', {
 
     this.ratk = ratk
 
-    this.material = new THREE.MeshNormalMaterial()
-    this.material.side = THREE.DoubleSide
+    // needed to work around Ammo.js shape generation bug.
+    this.adjustmentVector = new THREE.Vector3()
   },
 
   planeAdded(plane) {
@@ -25,21 +41,43 @@ AFRAME.registerSystem('xr-room-physics', {
     // this.el.setObject3D('plane', plane)
     plane.el = el
 
-    // create a mesh consisting of 2 copies of the plane, the rear 0.1m behind.
+    // take position & orientation from the plane
+    el.object3D.position.copy(plane.position)
+    el.object3D.quaternion.copy(plane.quaternion)
+
+    // create a mesh consisting of 2 copies of the plane, the rear 'depth' m behind.
     const mesh = new THREE.Group()
-    this.el.setObject3D('mesh', mesh)
-    mesh.position.copy(plane.position)
-    mesh.quaternion.copy(plane.quaternion)
+    el.setObject3D('mesh', mesh)
     const frontPlane = plane.planeMesh.clone()
     const rearPlane = plane.planeMesh.clone()
     mesh.add(frontPlane)
     mesh.add(rearPlane)
-    rearPlane.position.z += -0.1
-    frontPlane.material = this.material
-    rearPlane.material = this.material
+    rearPlane.position.y += this.data.depth
 
-    this.el.setAttribute('ammo-body', 'type: static')
-    this.el.setAttribute('ammo-shape', '')
+    if (this.data.debug) {
+      var randomColor = Math.floor(Math.random()*16777215)
+
+      const material = new THREE.MeshBasicMaterial( {color: randomColor, side: THREE.DoubleSide} );
+      frontPlane.material = material
+      rearPlane.material = material
+    }
+
+    if (this.driver === 'ammo') {
+
+      // Adjustment needed due to bug in Ammo shape generation from 2 parallel planes.
+      // Adjust mesh so planes are centered, and adjust plane element position to compensate.
+      mesh.position.y -= this.data.depth / 2
+      this.adjustmentVector.set(0, this.data.depth / 2, 0)
+      this.adjustmentVector.applyQuaternion(plane.quaternion)
+      el.object3D.position.add(this.adjustmentVector)
+      
+      el.setAttribute('ammo-body', 'type: static')
+      el.setAttribute('ammo-shape', '')
+    }
+    else {
+      el.setAttribute('static-body', '')
+    }
+    
     this.el.sceneEl.appendChild(el)
   },
 
