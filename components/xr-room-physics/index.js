4723521d96a23db211bd8f52bf1bca80e3844e3c
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-05-20 09:23:49 +0100

    Work in progress
    Not actually a good solution to room corners.  Can do better by raycasting to all other planes to determine whether to extend a plane or not.

--- components/xr-room-physics/index.js
+++ components/xr-room-physics/index.js
@@ -7,8 +7,52 @@ AFRAME.registerSystem('xr-room-physics', {
     // when enabled, planes are rendered with random colors to aid in debugging.
     debug: {default: false},
 
-    // depth (i.e. thickness) to use for walls and surfaces
+    // when debugging enabled, which planes to show.
+    showPlanes: {default: "all", oneOf: ["all", "horizontal", "vertical"]},
+
+    // depth (i.e. thickness) to use for walls and surfaces.
+    // Setting this to a larger value will reduce the likelihood of fast-moving 
+    // objects moving through planes, particularly when Continuous Collision Detection (CCD) 
+    // is not supported or is not enabled.
+
     depth: {default: 0.5},
+
+    // There is a further risk of leakage when CCD is unsupported / not enabled
+    // at the corners between the wall plans & the floor planes - see point marked "X".
+    // ----|
+    // wall|
+    //     | 
+    //     | 
+    //-----X-----------
+    //     | floor
+
+    // Also (viewed top down) this case...
+    // ----|
+    // wall|
+    //     | 
+    //     | 
+    //-----X-----------
+    //     | wall
+    //     |
+    // 
+    // We could solve this by enlarging planes, but there are some planes (e.g. a table surface) that
+    // we would not want to enlarge.
+    // 
+    // We should also consider non-convex rooms. So e.g.
+    //     |-----------|
+    //     |   wall    |
+    // ----|           |--------
+    // So we can't just extend walls horizontally...
+    // 
+    //
+    // Current solution is to define a floor height & ceiling height, and assume that any horizontal 
+    // planes above/below these levels should be scaled up horizontally, to reduce the risk of "leakage"
+    // at the joints between the walls and floor / ceiling.  !! DOESN'T WORK FOR WALL-WALL JOINTS...
+    //
+    // These don't need to match the exact floor & ceiling heights - they just need to ensure that
+    // furniture items such as tables don't get scaled up inappropriately.
+    floorHeight: {default: 0 },
+    ceilingHeight: {default: 2 }
   },
 
   init() {
@@ -67,9 +111,12 @@ AFRAME.registerSystem('xr-room-physics', {
     plane.el = el
 
     // convert plane material into a "hider material", so that it can occlude entities in a scene
-    // e/g/ when a ball rolls under a table.
-    plane.planeMesh.material.colorWrite = false
-    plane.planeMesh.material.side = THREE.DoubleSide
+    // e.g. when a ball rolls under a table.
+    // (but don't do this in debug mode, as it interferes with debug mesh rendering)
+    if (!this.data.debug) {
+      plane.planeMesh.material.colorWrite = false
+      plane.planeMesh.material.side = THREE.DoubleSide
+    }
 
     // take position & orientation from the plane
     el.object3D.position.copy(plane.position)
@@ -82,11 +129,49 @@ AFRAME.registerSystem('xr-room-physics', {
     const mesh = new THREE.Mesh(prismGeometry, plane.planeMesh.material)
     el.setObject3D('mesh', mesh)
 
-    if (this.data.debug) {
-      var randomColor = Math.floor(Math.random()*16777215)
+    // Concept:
+    // for each axis of the plane, extend by delta (0.1m?)
+    // raycast from camera to this delta.
+    // count intersections (front sides & back sides)
+    // if front intersections > back intersections, we can extend the plane in that direction...
+    // Repeat multiple times to verify this for various deltas...
+    // expand to maximum delta that we could safely extend to...
 
+/* Some code c/o ChatGPT that helps with this
+    const raycaster = new THREE.Raycaster();
+    const origin = new THREE.Vector3(0, 0, 0);
+    const direction = new THREE.Vector3(0, 0, -1).normalize();
+
+    raycaster.set(origin, direction);
+
+    const intersections = raycaster.intersectObject(mesh);
+    if (intersections.length > 0) {
+      const intersection = intersections[0];
+      const dot = raycaster.ray.direction.dot(intersection.face.normal);
+      if (dot < 0) {
+        // hit the back face of the geometry
+      } else {
+        // hit the front face of the geometry
+      }
+    }
+*/
+
+    if (plane.xrPlane.orientation === "horizontal" &&
+        (el.object3D.position.y <= this.data.floorHeight ||
+         el.object3D.position.y >= this.data.ceilingHeight)) {
+      // scale up floor & ceiling planes horizontally to reduce leakage at joints with walls,
+      // where Continuous Collision Detection (CCD) is not implemented.
+      //mesh.scale.set(2, 1, 2)
+    }
+
+    if (this.data.debug && 
+        (this.data.showPlanes === "all" || 
+         this.data.showPlanes === plane.xrPlane.orientation)) {
+
+      var randomColor = Math.floor(Math.random()*16777215)
       const material = new THREE.MeshBasicMaterial( {color: randomColor, side: THREE.DoubleSide} );
       mesh.material = material
+
     }
     else {
       mesh.visible = false
