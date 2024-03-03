79446fefab3a4b14a776023a71b5ff7a96881274
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2024-03-03 15:53:17 +0000

    Simple working dat-gui integration

--- components/dat-gui/index.js
+++ components/dat-gui/index.js
@@ -3,7 +3,29 @@ const dat = require('dat.gui');
 AFRAME.registerComponent('dat-gui', {
 
   init() {
-    
-  }
 
+    this.gui = new dat.GUI();
+    this.gui.domElement.classList.add('gui')
+
+    const components = Object.values(this.el.components)
+    components.forEach(component => this.addFolder(component))
+  },
+
+  addFolder(component) {
+
+    const componentName = component.attrName
+    const folder = this.gui.addFolder(componentName)
+
+    const data = component.data
+    const properties = Object.keys(data)
+
+    properties.forEach((prop) => {
+      const el = this.el
+      const propController = folder.add(data, prop)
+
+      propController.onChange(() => {
+        this.el.setAttribute(componentName, data)
+      })
+    })
+  }
 });
